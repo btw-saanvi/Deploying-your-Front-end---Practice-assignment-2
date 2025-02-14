cc6de8cdf1e79aa444172f67cfa7897c9700e7eb
btw-saanvi <Saanvigarg2020@gmail.com>
2025-02-14 09:39:42 +0530

    done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>HI KALVIUM</h1>
+      <p>THIS IS ASSIGNMENT</p>
+      <p>Deploying your Front-end - Practice assignment #2</p>
+    </div>
+  );
+}
+
+export default App;
+
+// function App() {
+//   return (
+//     <div style={styles.container}>
+//       <h1 style={styles.heading}>HI KALVIUM</h1>
+//       <p style={styles.text}>THIS IS ASSIGNMENT</p>
+//       <p style={styles.text}>Deploying your Front-end - Practice assignment #2</p>
+//     </div>
+//   );
+// }
+
+// const styles = {
+//   container: {
+//     textAlign: "center",
+//     marginTop: "50px",
+//     backgroundColor: "#f0f8ff",
+//     padding: "50px",
+//     borderRadius: "15px",
+//     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+//     maxWidth: "400px",
+//     marginLeft: "auto",
+//     marginRight: "auto",
+//   },
+//   heading: {
+//     color: "#2c3e50",
+//     fontSize: "28px",
+//   },
+//   text: {
+//     color: "#34495e",
+//     fontSize: "18px",
+//   },
+// };
+
+// export default App;
+
